df2e2ae60c94d4b66e0540d5c29cb490775eb085
Melissa02 <132373164+Melissa07001@users.noreply.github.com>
2025-08-06 00:19:19 +0200

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
 from .views import (
     AuthorListCreateView, AuthorDetailView,
     BookListCreateView, BookDetailView
@@ -10,10 +9,4 @@ urlpatterns = [
     path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
     path('books/', BookListCreateView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-=======
-from .views import hello
-
-urlpatterns = [
-    path('hello/', hello),
->>>>>>> c44adeb1cd238ee62f92fa0072aa93fd5902541e
 ]
