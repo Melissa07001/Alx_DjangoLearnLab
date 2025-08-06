605b98278c744d01a74764f290fd629ec06c4150
Melissa02 <132373164+Melissa07001@users.noreply.github.com>
2025-08-06 18:22:42 +0200

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,23 +1,9 @@
 from django.db import models
 
-<<<<<<< HEAD
-class Author(models.Model):
-    # The name of the author
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
+# Create your models here.
 class Book(models.Model):
-    # Title of the book
-    title = models.CharField(max_length=200)
-    # Year the book was published
-    publication_year = models.IntegerField()
-    # Link to the author (one author can have many books)
-    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
-=======
-# Create your models here.
->>>>>>> c44adeb1cd238ee62f92fa0072aa93fd5902541e
