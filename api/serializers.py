558b94068fdee19a78c551ecd51c144a68e51a42
Melissa02 <132373164+Melissa07001@users.noreply.github.com>
2025-08-06 18:23:21 +0200

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,8 @@
 from rest_framework import serializers
-from .models import Author, Book
+from .models import Book
+
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'publication_year', 'author']
-
-class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True, read_only=True)  # Nested books
-
-    class Meta:
-        model = Author
-        fields = ['id', 'name', 'books']
+        fields = '__ all__'
