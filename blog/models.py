cbbd7a76c96cd18e6247dbe3bc7fab6319a3baec
Melissa07001 <melissamkhize291@gmail.com>
2025-08-17 23:06:42 +0200

    Ensure Post model includes author and published_date

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  # set default user ID
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     published_date = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
 
